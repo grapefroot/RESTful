163fe3e542717236aa6fd374cb893919a98eef04
sad94 <salnikov.dmitri@gmail.com>
2014-07-03 12:22:34 +0400

    views work

---
+++ js/views/book.js
@@ -0,0 +1,20 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+    tagName: 'div',
+    className: 'bookContainer',
+    template: _.template( $('#bookTemplate').html() ),
+
+    render: function() {
+        this.$el.html(this.template( this.model.toJSON() ) );
+
+        return this;
+    },
+
+     renderBook: function( item ) {
+            var bookView = new app.BookView({
+                model: item
+            });
+            this.$el.append( bookView.render().el );
+        }
+});
